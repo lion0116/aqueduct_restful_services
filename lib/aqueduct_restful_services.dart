3c5473332c30294ff114c4fc7d1be3f7518e0201
Julius <zailongshi2013@gmail.com>
2020-05-30 16:17:49 +0800

    Initial commit

---
+++ lib/aqueduct_restful_services.dart
@@ -0,0 +1,11 @@
+/// aqueduct_restful_services
+///
+/// A Aqueduct web server.
+library aqueduct_restful_services;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
